ecc45606af4d84f78185a1513d2571426094a6de
Maharshi Sinha <72745563+maharshi-sinha@users.noreply.github.com>
2026-03-23 20:13:55 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 
 http.createServer((req, res) => {
-  res.write("Hello from Docker App (testing in EC2, now testing Jenkins webhooks in EC2)");
+  res.write("Hello from Docker App (now testing Jenkins webhooks in EC2)");
   res.end();
 }).listen(3000);
